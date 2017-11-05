574cf9dbba44394cbd13206ee92585bbebff3193
suomiy <suomiy@gmail.com>
2017-11-05 11:52:45 +0100

    Part 5.2 HTTP server: print data from url

--- server/my_http_request_handler.py
+++ server/my_http_request_handler.py
@@ -1,7 +1,10 @@
 from http.server import BaseHTTPRequestHandler
+from urllib.parse import urlparse,parse_qs
+from functools import reduce
 
 
 class MyHTTPRequestHandler(BaseHTTPRequestHandler):
+
     # GET
     def do_GET(self):
         # Send response status code
@@ -11,7 +14,12 @@ class MyHTTPRequestHandler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
+        parsed_url = urlparse(self.path)
+        query_dict = parse_qs(parsed_url.query)
+        query_view = reduce(lambda x, key: x + f'<li>{key}: {query_dict[key]}</li>', query_dict, '')
+
         # Send message back to client
-        message = "<h1>Hello world!</h1"
+        message = f'<h1>Hello world! called from {parsed_url.path}</h1>' + \
+                  f'<ul>{query_view}</ul>'
         # Write content as utf-8 data
         self.wfile.write(bytes(message, "utf8"))
